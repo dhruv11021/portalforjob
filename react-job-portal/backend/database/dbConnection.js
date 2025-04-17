ae7b399c478b96821baee68678b5a4c649bbbb7b
dhruv11021 <dhruv.gla1_cs22@gla.ac.in>
2025-04-17 13:37:43 +0530

    some changes

--- react-job-portal/backend/database/dbConnection.js
+++ react-job-portal/backend/database/dbConnection.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose"; //just mongoose import!
-import dotenv from "dotenv"
-dotenv.config()
+// database/dbConnection.js
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-//Database connection here!
- const dbConnection  = ()=>{
-    mongoose.connect(process.env.DB_URL,{
-       dbName: "Job_Portal"
+dotenv.config();
 
-    }).then(()=>{ //agar connect ho jaye toh!
-       console.log("MongoDB Connected Sucessfully !")
-    }).catch((error)=>{
-        console.log(`Failed to connect ${error}`)
-    })
-    
-}
+const dbConnection = () => {
+  mongoose.connect(process.env.MONGO_URI, {
+    dbName: 'Job_Portal',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected Successfully!'))
+  .catch((error) => console.log(`Failed to connect: ${error}`));
+};
+
 export default dbConnection;
