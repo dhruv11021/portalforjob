6fa235254bd9863e2bb18dd7b2a646318e1c4b06
dhruv11021 <dhruv.gla1_cs22@gla.ac.in>
2025-04-17 14:07:24 +0530

    Serve frontend from Express

--- react-job-portal/backend/app.js
+++ react-job-portal/backend/app.js
@@ -14,11 +14,12 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
+    origin: process.env.FRONTEND_URL || "https://portalforjob.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
 
 app.use(cookieParser());
 app.use(express.json());
