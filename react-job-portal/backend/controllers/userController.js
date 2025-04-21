6d61cda4e7cf7319a50d41f824f0d17985829f49
dhruv11021 <dhruv.gla1_cs22@gla.ac.in>
2025-04-21 22:52:11 +0530

    commits

--- react-job-portal/backend/controllers/userController.js
+++ react-job-portal/backend/controllers/userController.js
@@ -24,24 +24,24 @@ export const register = catchAsyncErrors(async (req, res, next) => {
 
 export const login = catchAsyncErrors(async (req, res, next) => {
   const { email, password, role } = req.body;
+
   if (!email || !password || !role) {
-    return next(new ErrorHandler("Please provide email ,password and role !"));
+    return next(new ErrorHandler("Please provide email, password and role!", 400));
   }
-  const user = await User.findOne({ email }).select("+password");
+
+  const user = await User.findOne({ email, role }).select("+password");
   if (!user) {
-    return next(new ErrorHandler("Invalid Email Or Password.", 400));
+    return next(new ErrorHandler("Invalid email, password or role!", 400));
   }
+
   const isPasswordMatched = await user.comparePassword(password);
   if (!isPasswordMatched) {
-    return next(new ErrorHandler("Invalid Email Or Password !", 400));
+    return next(new ErrorHandler("Invalid email or password!", 400));
   }
-  if (user.role !== role) {
-    return next(
-      new ErrorHandler(`User with provided email and ${role} not found !`, 404)
-    );
-  }
-  sendToken(user, 201, res, "User Logged In Sucessfully !");
+
+  sendToken(user, 201, res, "User Logged In Successfully!");
 });
+
 
 export const logout = catchAsyncErrors(async (req, res, next) => {
   res
